6783f48dff5080b6176fb552f22e681a1bbc358f
Petani Kode <muhammadrazin14@gmail.com>
2022-12-08 16:21:44 +0700

    Menambahkan halaman tes mental health

---
+++ pages/Tes Mental Health/scripts/data.js
@@ -0,0 +1,170 @@
+var jsonData = [
+    {
+        "q" : "Saya sulit menenangkan diri",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya merasa mulut saya kering",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya tidak dapat merasakan perasaan yang positif",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi",
+    },
+    {
+        "q" : "Saya mengalami kesulitan bernafas",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas",
+    },
+    {
+        "q" : "Saya merasa sulit untuk memulai sesuatu",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi",
+    },
+    {
+        "q" : "Saya cenderung bereaksi berlebihan pada situasi tertentu",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya mengalami gemetar (misalnya, di tangan)",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya merasa bahwa saya menggunakan banyak energi untuk cemas",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya mengkhawatirkan situasi yang membuat saya panik dan tampak bodoh",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya merasa tidak memiliki masa depan",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi"
+    },
+    {
+        "q" : "Saya merasa semakin gelisah",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya sulit untuk tenang/rileks",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya merasa sedih dan murung",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi"
+    },
+    {
+        "q" : "Saya tidak toleran terhadap apapun yang menghalangi saya untuk menyelesaikan hal yang sedang saya kerjakan",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya merasa mudah panik",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya tidak antusias terhadap sesuatu",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi"
+    },
+    {
+        "q" : "Saya merasa tidak berharga sebagai seseorang",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi"
+    },
+    {
+        "q" : "Saya merasa mudah tersinggung",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Stress"
+    },
+    {
+        "q" : "Saya menyadari jantung saya berdebar kencang/denyut jantung meningkat walau tanpa aktivitas fisik",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya merasa takut tanpa alasan yang jelas",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Cemas"
+    },
+    {
+        "q" : "Saya merasa bahwa hidup ini tidak berarti",
+        "skor0" : "0 (Tidak Pernah)",
+        "skor1" : "1 (Kadang)",
+        "skor2" : "2 (Sering)",
+        "skor3" : "3 (Hampir Selalu)",
+        "category" : "Depresi"
+    }
+];
---
+++ pages/Tes Mental Health/scripts/index.js
@@ -0,0 +1,53 @@
+var i = 0;
+var count = 0;
+
+generate(0);
+
+function generate(index) {
+    document.getElementById("test").innerHTML = jsonData[index].q;
+    document.getElementById("skorr0").innerHTML = jsonData[index].skor0;
+    document.getElementById("skorr1").innerHTML = jsonData[index].skor1;
+    document.getElementById("skorr2").innerHTML = jsonData[index].skor2;
+    document.getElementById("skorr3").innerHTML = jsonData[index].skor3;
+}
+
+function countAnswer() {
+    if (document.getElementById("skor1").checked && jsonData[i].skor1 && jsonData[i].category=="Depresi") {
+        countDepresi++;
+    }
+    if (document.getElementById("skor2").checked && jsonData[i].skor2 && jsonData[i].category=="Depresi") {
+        countDepresi+=2;
+    }
+    if (document.getElementById("skor3").checked && jsonData[i].skor3 && jsonData[i].category=="Depresi") {
+        countDepresi+=3;
+    }
+    if (document.getElementById("skor1").checked && jsonData[i].skor1 && jsonData[i].category=="Cemas") {
+        countCemas++;
+    }
+    if (document.getElementById("skor2").checked && jsonData[i].skor2 && jsonData[i].category=="Cemas") {
+        countCemas+=2;
+    }
+    if (document.getElementById("skor3").checked && jsonData[i].skor3 && jsonData[i].category=="Cemas") {
+        countCemas+=3;
+    }
+    if (document.getElementById("skor1").checked && jsonData[i].skor1 && jsonData[i].category=="Stress") {
+        countStress++;
+    }
+    if (document.getElementById("skor2").checked && jsonData[i].skor2 && jsonData[i].category=="Stress") {
+        countStress+=2;
+    }
+    if (document.getElementById("skor3").checked && jsonData[i].skor3 && jsonData[i].category=="Stress") {
+        countStress+=3;
+    }
+    i++;
+    if(jsonData.length-1 < i){
+        document.write("Skor anda adalah : ");
+        document.write("<br>");
+        document.write("Depresi = " + countDepresi);
+        document.write("<br>");
+        document.write("Cemas = " + countCemas);
+        document.write("<br>");
+        document.write("Stress = " + countStress);
+    }
+    generate(i);
+}
