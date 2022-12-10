a30809bd3df962d46148229e11f7a89dc74e975c
Petani Kode <muhammadrazin14@gmail.com>
2022-12-11 03:09:07 +0700

    Menampilkan 4 artikel terkini dari API tribun di halaman index

--- scripts/fetch news API index.js
+++ scripts/fetch news API index.js
@@ -3,35 +3,44 @@ const form = document.querySelector('form');
 const newsContainer = document.querySelector('#news-container');
 
 async function getData() {
-    const fetchData = await fetch(
-        'https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=fab180adfd804228917c512c4599d0d4'
-    );
-    const response = await fetchData.json();
+    try {
+        const response = await fetch(
+            'https://api-berita-indonesia.vercel.app/tribun/kesehatan/'
+        );
+        DATA = await response.json();
 
-    let count = 0;
-
-    for (const article of response.articles) {
-        if (count === 4) {
-            break;
+        console.log('DATA API nya', DATA);
+        if (newsContainer === null) {
+            console.error('Artikel tidak dapat ditampilkan');
+            return;
         }
 
-        newsContainer.innerHTML += templateNews(article);
-        count++;
-    }
+        const DATAArray = Array.from(DATA.data.posts);
 
-    DATA = response;
+        for (let i = 0; i < 4; i++) {
+            const article = DATAArray[i];
+            newsContainer.innerHTML += templateNews(article);
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function templateNews(data) {
+    let image = '';
+    if (data.thumbnail !== null) {
+        image = `<img src="${data.thumbnail}" alt="">`;
+    }
+
     return `
     <div class="col-12 col-md-6 col-lg-3 mt-4" id="news-item">
         <div class="card">
-            <img src="${data.urlToImage}" alt="">
-            <a href="${data.url}" class="mt-5">
+            ${image}
+            <a href="${data.link}" class="mt-5">
                 <h6>${data.title}</h6>
             </a>
             <p class="article-categories">${data.description}</p>
-            <p class="article-time">${data.publishedAt} by ${data.author}</p>
+            <p class="article-time">${data.pubDate}</p>
         </div>
     </div>
     `;
