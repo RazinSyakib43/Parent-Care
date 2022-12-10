4542f0f7f07c0b1aae01133eae751c2ece9984ff
Petani Kode <muhammadrazin14@gmail.com>
2022-12-11 02:49:08 +0700

    kembalikan ke api sebelumnya

--- scripts/fetch news API index.js
+++ scripts/fetch news API index.js
@@ -1,16 +1,16 @@
-let DATA = {};
+let DATA = null;
 const form = document.querySelector('form');
 const newsContainer = document.querySelector('#news-container');
 
 async function getData() {
     const fetchData = await fetch(
-        'https://api-berita-indonesia.vercel.app/tribun/kesehatan/'
+        'https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=fab180adfd804228917c512c4599d0d4'
     );
     const response = await fetchData.json();
 
     let count = 0;
 
-    for (const article of response.data) {
+    for (const article of response.articles) {
         if (count === 4) {
             break;
         }
@@ -22,16 +22,16 @@ async function getData() {
     DATA = response;
 }
 
-function templateNews(datas) {
+function templateNews(data) {
     return `
     <div class="col-12 col-md-6 col-lg-3 mt-4" id="news-item">
         <div class="card">
-            <img src="${datas.posts.thumbnail}" alt="">
-            <a href="${datas.posts.link}" class="mt-5">
-                <h6>${datas.posts.title}</h6>
+            <img src="${data.urlToImage}" alt="">
+            <a href="${data.url}" class="mt-5">
+                <h6>${data.title}</h6>
             </a>
-            <p class="article-categories">${datas.posts.description}</p>
-            <p class="article-time">${datas.posts.pubDate}</p>
+            <p class="article-categories">${data.description}</p>
+            <p class="article-time">${data.publishedAt} by ${data.author}</p>
         </div>
     </div>
     `;
